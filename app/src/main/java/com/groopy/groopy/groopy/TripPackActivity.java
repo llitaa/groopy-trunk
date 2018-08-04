959f667cb367723d119061c7512a695acf03cdb6
Anton Lytvynenko <antonlytvynenko@gmail.com>
2018-08-04 16:19:25 +0200

    first commit

---
+++ app/src/main/java/com/groopy/groopy/groopy/TripPackActivity.java
@@ -0,0 +1,15 @@
+package com.groopy.groopy.groopy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TripPackActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_pack);
+    }
+}
