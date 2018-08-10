eab80952edf8d64b4d89bf9e5c2038cde45a5a6d
Anton Lytvynenko <antonlytvynenko@gmail.com>
2018-08-10 22:33:12 +0200

    Added missing PackItemView file

---
+++ app/src/main/java/com/groopy/groopy/groopy/ui/PackItemView.java
@@ -0,0 +1,34 @@
+package com.groopy.groopy.groopy.ui;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.groopy.groopy.groopy.R;
+import com.groopy.groopy.groopy.model.PackItem;
+
+public class PackItemView extends LinearLayout
+{
+    public PackItemView(Context context)
+    {
+        this(context, null);
+    }
+
+    public PackItemView(Context context, @Nullable AttributeSet attrs)
+    {
+        super(context, attrs);
+        inflate(getContext(), R.layout.pack_item_view, this);
+    }
+
+    public void setSourceData(PackItem packItem)
+    {
+        TextView nameView = findViewById(R.id._itemName);
+        nameView.setText(packItem.getName());
+        TextView decriptionView = findViewById(R.id._itemDescription);
+        decriptionView.setText(packItem.getDescription());
+    }
+}
